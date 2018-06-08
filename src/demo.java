0aafb3b935e5840584ae08f3b1651405ba35a220
goodTool <wjk_you@163.com>
2018-06-08 20:14:55 +0800

    2018

--- src/demo.java
+++ src/demo.java
@@ -4,6 +4,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("2017");
 		System.out.println("20182018");
 		System.out.println("firstfirst");
 		System.out.println("second");
