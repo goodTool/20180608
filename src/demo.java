2eddcd0341417a8331d4e7a0dfdaf264bb9271dc
goodTool <wjk_you@163.com>
2018-06-08 20:22:03 +0800

    2018

--- src/demo.java
+++ src/demo.java
@@ -5,7 +5,7 @@ public class demo {
 		// TODO Auto-generated method stub
 		
 		System.out.println("2017");
-		System.out.println("20182018");
+		System.out.println("2017");
 		System.out.println("20182018");
 		System.out.println("firstfirst");
 		System.out.println("second");
