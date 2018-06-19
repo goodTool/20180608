3379a7fcfa569a00c83aa3f6398f2cabb75fc8aa
goodTool <wjk_you@163.com>
2018-06-19 13:13:47 +0800

    解决冲突

--- src/demo.java
+++ src/demo.java
@@ -4,8 +4,13 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("1111111111111111");
-		System.out.println("branch2  2222");
+		System.out.println("2017");
+		System.out.println("2017");
+		System.out.println("2017");
+		System.out.println("20182018");
+		System.out.println("firstfirst");
+		System.out.println("second");
+		System.out.println("four");
 
 	}
 
